21e24a2c64ac646a1ba58982aeca2f628556525f
Lucas Antunes <lucasdanielantunes@gmail.com>
2021-12-29 20:26:51 -0300

    Models e Mapeamentos

---
+++ Gerenciador_Condominios/Gerenciador_Condominios.BLL/Models/Pagamento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciador_Condominios.BLL.Models
+{
+    public class Pagamento
+    {
+        public int PagamentoId { get; set; }
+
+        public string UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+
+        public int AluguelId { get; set; }
+        public Aluguel Aluguel { get; set; }
+
+        public DateTime? DataPagamento { get; set; }
+
+        public StatusPagamento Status { get; set; }
+    }
+
+    public enum StatusPagamento
+    {
+        Pago, Pendente, Atrasado
+    }
+}
---
+++ Gerenciador_Condominios/Gerenciador_Condominios.DAL/Mapeamentos/FuncaoMap.cs
@@ -0,0 +1,47 @@
+﻿using Gerenciador_Condominios.BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciador_Condominios.DAL.Mapeamentos
+{
+    public class FuncaoMap : IEntityTypeConfiguration<Funcao>
+    {
+        public void Configure(EntityTypeBuilder<Funcao> builder)
+        {
+            builder.Property(f => f.Id).ValueGeneratedOnAdd();
+            builder.Property(f => f.Descricao).IsRequired().HasMaxLength(30);
+
+            builder.HasData(
+                new Funcao
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Morador",
+                    NormalizedName = "MORADOR",
+                    Descricao = "Morador do Prédio"
+                },
+
+                new Funcao
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Sindico",
+                    NormalizedName = "SINDICO",
+                    Descricao = "Síndico do Prédio"
+                },
+
+                new Funcao
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Administrador",
+                    NormalizedName = "ADMINISTRADOR",
+                    Descricao = "Administrador do Prédio"
+                });
+
+            builder.ToTable("Funcoes");
+        }
+    }
+}
