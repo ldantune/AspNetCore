08a5c3706fed31fd6dc652572a6b7a3df466e0fd
Lucas Antunes <lucasdanielantunes@gmail.com>
2021-12-29 18:27:41 -0300

    Criação do projeto

---
+++ Gerenciador_Condominios/Gerenciador_Condominios/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gerenciador_Condominios.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Gerenciador_Condominios/Gerenciador_Condominios/Program.cs
@@ -0,0 +1,20 @@
+
+namespace Gerenciador_Condominios
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
