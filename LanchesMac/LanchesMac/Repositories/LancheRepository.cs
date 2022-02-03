8ca87c87427ea3370acb84e7010e60f92cc79dfb
Lucas Antunes <lucasdanielantunes@gmail.com>
2022-02-03 20:47:27 -0300

    Carrinho de Compras

--- LanchesMac/LanchesMac/Repositories/LancheRepository.cs
+++ LanchesMac/LanchesMac/Repositories/LancheRepository.cs
@@ -18,18 +18,11 @@ namespace LanchesMac.Repositories
         public IEnumerable<Lanche> LanchePreferido => _context.Lanches.Where(
             p => p.IsLanchePreferido).Include(c => c.Categoria);
 
-        IEnumerable<Lanche> ILancheRepository.Lanches => throw new NotImplementedException();
-
-        IEnumerable<Lanche> ILancheRepository.LanchePreferido => throw new NotImplementedException();
 
         public Lanche GetLancheById(int lancheId)
         {
             return _context.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
         }
 
-        Lanche ILancheRepository.GetLancheById(int lancheId)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
